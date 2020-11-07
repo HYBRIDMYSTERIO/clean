99d5475bf8931dc489b857624a1fca1ca9fab0ac
HYBRIDMYSTERIO <73470518+HYBRIDMYSTERIO@users.noreply.github.com>
2020-11-07 19:09:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,53 +1,69 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
-var engine , world , ground , paper, dustbin_1, dustbin_2, dustbin_3;
+var engine, world;
+var box1, pig1, background_1 ;
 
-function preload()
-{
-	
+function preload(){
+    background_1  = loadImage("sprites/bg.png");
 }
 
-function setup() {
-	createCanvas(800, 700);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-
-	
-	ground = new Ground(400,540,800,10);
-	paper = new Paper(100,200,40);
-	dustbin_1 = new Dustbin(500,530,150,10);
-	dustbin_2 = new Dustbin(430,500,10,60);
-	dustbin_3 = new Dustbin(570,500,10,60);
-
-	Engine.run(engine);
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
   
+   
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/8);
+
+    log6 = new Log(800,100,150, PI/2);
+    pig4 = new Pig(800,80);
+
+    bird = new Bird(100,100);
+
 }
 
+function draw(){
+    
+    background(background_1);
+    
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
 
-function draw() {
-  Engine.update(engine);
-  rectMode(CENTER);
-  background(0);
-  ground.displayground();
-  paper.displaypapers();
-  dustbin_1.displaydustbin();
-  dustbin_2.displaydustbin();
-  dustbin_3.displaydustbin();
-  drawSprites();
- 
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+    log6.display();
+    pig4.display();
+    
+
+    bird.display();
 }
-
-function keyPressed(){
-	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position, {x:115,y:-115});
-	}
-}
-
